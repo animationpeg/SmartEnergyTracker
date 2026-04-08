9be1db55241115f54789e1c07b071e7a036439ad
animationpeg <jeffers800@hotmail.co.uk>
2026-04-08 16:00:51 +0100

    Created Meter and MeterReading models
    - Defined two Models Meter and MeterReading, with a many-to-one relationship. One Meter will have many MeterReadings.
    - MeterReading contains the timestamp of the reading and the kwh
    - set def __str__(self) method to define an easily readable string output for the MeterReading

--- energy/models.py
+++ energy/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+## Base class of an Energy Meter
+class Meter(models.Model):
+    # The name, as a string with a set maximum length
+    name = models.CharField(max_length=100)
+
+    # Define how this object is displayed as a string by django
+    def __str__(self):
+        return self.name
+
+## Every Meter will have a Meter Reading, which will consist of time of the reading, and the kwh reading
+class MeterReading(models.Model):
+    # Define the relationship between the MeterReading and the Meter classes.
+    # "related_name=" allows a Meter object to easily look/access properties from the MeterReading
+    meter = models.ForeignKey(Meter, on_delete=models.CASCADE, related_name="readings")
+    timestamp = models.DateTimeField()
+    kwh = models.FloatField()
+
+    # Define how this object is displayed as a string by django
+    def __str__(self):
+        return f"{self.meter.name} - {self.timestamp} - {kwh} kwh"
